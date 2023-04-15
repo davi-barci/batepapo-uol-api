61a8977a7ded0f3abace604dab5c268c178b465d
Davi Barci <davibarci0208@gmail.com>
2023-04-15 01:13:42 -0300

    Finalizado método post que publica uma mensagem enviada pelo usuário

--- src/app.js
+++ src/app.js
@@ -64,5 +64,41 @@ app.get("/participants", async (req, res) => {
     }
 });
 
+app.post("/messages", async (req, res) => {
+    const user = req.headers.user;
+    const { to, text, type } = req.body;
+
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message').required()
+    });
+
+    const validation = messageSchema.validate(req.body, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const resp = await db.collection("participants").findOne({ name: user });
+
+        if (!resp) return res.status(409).send("Este usuário não está online!");
+
+        await db.collection("messages").insertOne({
+            from: user, 
+            to: to, 
+            text: text, 
+            type: type, 
+            time: dayjs().format('HH:mm:ss')
+        });
+
+        return res.sendStatus(201);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+});
+
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
